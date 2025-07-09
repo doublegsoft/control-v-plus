8f150c8411ba4a170c67393f33c8bf96b689ef18
Christian Gann <guo.guo.gan@gmail.com>
2025-07-09 13:10:56 +0800

    cross-platform support

---
+++ src/mac-10.11/main.m
@@ -0,0 +1,15 @@
+/*                  _             _
+**   ___ ___  _ __ | |_ _ __ ___ | |    __   __ _
+**  / __/ _ \| '_ \| __| '__/ _ \| |____\ \ / /| |_
+** | (_| (_) | | | | |_| | | (_) | |_____\ V /_   _|
+**  \___\___/|_| |_|\__|_|  \___/|_|      \_/  |_|
+**
+*/
+#import <Cocoa/Cocoa.h>
+
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+    return NSApplicationMain(argc, (const char **)argv);
+  }
+}
